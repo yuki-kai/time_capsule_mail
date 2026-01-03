f825bf8d94c27f3db04e784e25a1a79e35e19826
石原 祐希 <issi0430bjc@gmail.com>
2026-01-03 13:42:22 +0900

    403,404エラー時はホームページにリダイレクトするようCloudFrontに設定を追加

--- infra/modules/static_site/cloudfront.tf
+++ infra/modules/static_site/cloudfront.tf
@@ -48,6 +48,19 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2021"
   }
+
+  custom_error_response {
+    error_code             = 403
+    response_code          = 200
+    response_page_path     = "/"
+    error_caching_min_ttl  = 0
+  }
+  custom_error_response {
+    error_code             = 404
+    response_code          = 200
+    response_page_path     = "/"
+    error_caching_min_ttl  = 0
+  }
 }
 
 // OAC
